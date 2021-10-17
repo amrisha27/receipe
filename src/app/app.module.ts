e8dca3a3053eda7bab56d9890b6610ff34c5abc8
Amrisha <pamrisha7n7@gmail.com>
2021-10-17 16:02:23 +0530

    adding header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,16 @@ import { HttpClientModule } from "@angular/common/http";
 
 
 import { AppComponent } from "./app.component";
-
+import {HeaderComponent} from './header/header.component';
 
 @NgModule({
   declarations: [
-
+    HeaderComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     HttpClientModule,
+
   ],
   bootstrap: [AppComponent],
 })
